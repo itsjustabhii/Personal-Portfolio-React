7af99f716a6015291b48581ea435a1110344c290
itsjustabhii <itsjustabhii@gmail.com>
2023-02-13 13:59:04 +0530

    modified nav-icon typo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import {useState, useEffect} from "react"
 import {Navbar, Container, Nav} from "react-bootstrap"
 import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-Icon1.svg'
-import navIcon2 from '../assets/img/nav-Icon2.svg'
-import navIcon3 from '../assets/img/nav-Icon3.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 
 
 export const NavBar = () => {
