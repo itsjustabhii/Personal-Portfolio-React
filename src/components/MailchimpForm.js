485540c3dfaf76774f443f2539e8cb31689016e2
itsjustabhii <itsjustabhii@gmail.com>
2023-02-13 13:58:32 +0530

    corrected Newsletter import

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,5 +1,5 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
-import { Newsletter } from "./Newsletter";
+import { Newsletter } from "./NewsLetter";
 
 export const MailchimpForm = () => {
   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
