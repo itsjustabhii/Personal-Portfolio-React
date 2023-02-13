c18efe6edf1aa58fee9623776b85bfb01bbf6749
itsjustabhii <itsjustabhii@gmail.com>
2023-02-13 13:20:02 +0530

    added TrackVisibility

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import {useState, useEffect} from "react"
 import {Container, Row, Col} from "react-bootstrap"
 import {ArrowRightCircle} from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
+import 'animate.css'
+import TrackVisibility from 'react-on-screen'
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0)
@@ -45,10 +47,15 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className='tagline'>Welcome to my Portfolio</span>
-                        <h1>{`Hi, I'm a Web Developer! `}<span className='wrap'>{text}</span></h1>
-                        <p>Lorem Ipsum</p>
-                        <button onClick={()=> console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                        <TrackVisibility>
+                        {({isVisible}) => 
+                        <div className={isVisible ? "animated__animated animate__fadeIn" : ""}>
+                            <span className='tagline'>Welcome to my Portfolio</span>
+                            <h1>{`Hi, I'm a Web Developer! `}<span className='wrap'>{text}</span></h1>
+                            <p>Lorem Ipsum</p>
+                            <button onClick={()=> console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                        </div> }
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img"></img>
